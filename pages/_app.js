42eb584e796c329d5bb64d8ac9201c496e875967
Ana Judith <ana-judth@hotmail.com>
2023-08-25 22:24:04 -0300

    ✅ create pages config

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import App from "next/app";
 import GlobalStyle from "../src/theme/GlobalStyle";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
--- pages/faq.js
+++ pages/faq.js
@@ -1,12 +1,25 @@
-import React from 'react'
-import Link from '../src//components/Link'
+// faq.js
+import FAQScreen from "../src/screens/FAQScreen";
 
-export default function FaqPage() {
-  return (
-    <div>
-    <h1>Alura Case - Pagina de perguntas</h1>
-    <Link href='/'>
-    Ir Para o faq</Link>
-</div>
-  )
+export default FAQScreen;
+
+export async function getStaticProps() {
+  console.log("Em modo DEV, sempre roda! A cada acesso");
+  console.log("Roda SOMENTE em build time");
+  const FAQ_API_URL =
+    "https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json";
+  const faq = await fetch(FAQ_API_URL)
+    .then((respostaDoServidor) => {
+      return respostaDoServidor.json();
+    })
+    .then((resposta) => {
+      return resposta;
+    });
+
+  return {
+    props: {
+      qualquercoisa: "que eu passar aqui",
+      faq,
+    },
+  };
 }
